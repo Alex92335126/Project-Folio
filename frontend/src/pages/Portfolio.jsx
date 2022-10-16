a5fe6aaaadd3d324ff23b164630cd2b2bf8d8d22
alex <alex92335126@gmail.com>
2022-10-16 16:49:00 +0800

    10-16 commit

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -41,62 +41,78 @@ export default function Portfolio() {
 
   return (
     <>
-      <div className="portfolio" style={{ color: "orange" }}>
+    <div>
+      <div className="portfolio" style={{ color: "orange", display: ""}}>
         <h2 className="py-4">Welcome Back {user.firstName}!</h2>
         {/* {cashTotal} */}
+        <div className="d-flex">
+            <div className="col-md-6">
+                <table>
+                <thead>
+                    <h2> Stock Account</h2>
+                    <tr>
+                    <th>Symbol</th>
+                    <th>Number of share</th>
+                    <th>Market price</th>
+                    <th>Current Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {assetList.assetPortfolio.map((asset) => (
+                    <tr key={asset.id}>
+                        <td>{asset.symbol}</td>
+                        <td>{asset.num_shares}</td>
+                        <td>{asset.sharePrice}</td>
+                        <td>{asset.amount}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+                <div className="py-1" />
+                <table>
+                <h2 className= "py-4"> Cash Account </h2>
+                <div>
+                <tbody>
+                    <tr>
+                    <td style={{width: "510px"}}>
+                        <strong >Cash Balance</strong>
+                    </td>
+                    <td>{assetList.cashBal}</td>
+                    </tr>
+                </tbody>
+                </div>
+                <div>
+                <h2 className= "py-4">Total Asset</h2>
+                <tbody>
+                <tr>
+                    <td style={{width: "510px"}}>
+                        <strong>Total (Cash+Stock)</strong>
+                    </td>
+                    <td>{assetList.totalBal}</td>
+                    </tr>
+                </tbody>  
+                </div>
+                </table>
+            </div>
+            <div className="py-4" />
+            <div className="col-md-6">
+                <div>
+                <BuySell />
+                </div>
+                
+                <div style={{width: "400px"}}>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Number of share</th>
-              <th>Market price</th>
-              <th>Current Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {assetList.assetPortfolio.map((asset) => (
-              <tr key={asset.id}>
-                <td>{asset.symbol}</td>
-                <td>{asset.num_shares}</td>
-                <td>{asset.sharePrice}</td>
-                <td>{asset.amount}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="py-4" />
-
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <strong >Cash Balance</strong>
-              </td>
-              <td>{assetList.cashBal}</td>
-            </tr>
-            <tr>
-              <td>
-                <strong>Total (Cash+Stock)</strong>
-              </td>
-              <td>{assetList.totalBal}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="py-2" />
-        <div>
-          <BuySell />
+                <PieChart
+                        labelStyle={{
+                            fontSize: '20px'
+                    }}
+                />
+            </div>
+            
         </div>
-        
-        <div style={{width: "400px"}}>
-
-        <PieChart
-                labelStyle={{
-                    fontSize: '20px'
-               }}
-         />
         </div>
       </div>
+    </div>
     </>
   );
 }
