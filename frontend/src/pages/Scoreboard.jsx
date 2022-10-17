77264bb58f52de35c3543ad797d525f0268da1b4
alex <alex92335126@gmail.com>
2022-10-17 22:22:17 +0800

    NFT

--- frontend/src/pages/Scoreboard.jsx
+++ frontend/src/pages/Scoreboard.jsx
@@ -1,9 +1,49 @@
-import {useState} from "react"
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function Login() {
 
 return (
-    <h1>Hello </h1>
-)
+    <div className= "scoreboard" style={{color: "#FFA500"}}>
+        <Card style={{ width: '18rem', height: "30rem", backgroundColor: "black"}}>
+        <Card.Img variant="top" src="/Gold.png" />
+        <Card.Body>
+            <Card.Title>Champion</Card.Title>
+            <Card.Text>
+            NFT GOLD PRIZE
+            </Card.Text>
+        </Card.Body>
+        </Card>
+        
+        <Card style={{ width: '18rem', height: "30rem", backgroundColor: "black"}}>
+        <Card.Img variant="top" src="/Silver.png" />
+        <Card.Body>
+            <Card.Title>2nd Runner Up</Card.Title>
+            <Card.Text>
+            NFT SILVER PRIZE
+            </Card.Text>
+        </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem', height: "30rem", backgroundColor: "black" }}>
+        <Card.Img variant="top" src="/Bronze.png" />
+        <Card.Body>
+            <Card.Title>3rd Runner Up</Card.Title>
+            <Card.Text>
+            NFT BRONZE PRIZE
+            </Card.Text>
+        </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem', height: "30rem", backgroundColor: "black" }}>
+        <Card.Img variant="top" src="/number4-10.png" />
+        <Card.Body>
+            <Card.Title>4-10 Runner Up</Card.Title>
+            <Card.Text>
+            NFT 4-10 RUNNER UP PRIZE
+            </Card.Text>
+        </Card.Body>
+        </Card>
+
+    </div>
+    )
 }
