91b88354d5aeb48341f9bc35a4a477c7955aa012
alice <aliceng223@gmail.com>
2022-10-18 21:50:36 +0800

    update contract

--- backend/contracts/NftToken.sol
+++ backend/contracts/NftToken.sol
@@ -1,35 +1,29 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Gamefolio is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    Counters.Counter private _tokenSupply;
 
-    constructor(string memory _name , string memory _symbol) ERC721(_name, _symbol) {}
+    constructor() ERC721("Gamefolio", "GMF") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _tokenSupply.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        _approve(msg.sender, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
-        _tokenSupply.decrement();
     }
 
     function tokenURI(uint256 tokenId)
@@ -39,9 +33,5 @@ contract MyToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
-    }
-
-    function getTotalSupply() public view returns(uint256) {
-        return _tokenSupply.current();
     }
 }
