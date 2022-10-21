09cada52c12a33dd635c848d416f9d38bd75ad68
alex <alex92335126@gmail.com>
2022-10-22 01:37:46 +0800

    game css updated

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -6,14 +6,14 @@ export default function Game() {
 return (
     <>
 
-    <div className= "scoreboard" style={{color: "#FFA500", padding: "60px"}}>
+    <div className= "game" style={{color: "#FFA500", padding: "30px"}}>
     <div>
         <h2 style={{textDecorationLine: 'underline', marginBottom: "20px"}}>Game Instruction:  </h2> 
         <h5 style={{width: "18rem"}}>Gamefolio is a Virtual Stock Market Simulator which provides real time stock price and news  which allows you to trade your favorite stocks. With the user friendly interface, you can show your stock picking skills and creating your own portfolio for winning the NFT and other prizes . You start with $100k and can buy and sell stocks in realtime from the US stock exchange and the game will start from 1st, Jan,  2023. GOOD LUCK! </h5>
     </div>
 
        
-        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black"}}>
+        <Card className='gamecard' style={{ width: '18rem', height: "35rem", backgroundColor: "black", borderStyle: "solid", borderColor: "orange", borderWidth: "5px"}}>
         <Card.Img variant="top" src="/Gold.png" />
         <Card.Body>
             <Card.Title>Champion</Card.Title>
@@ -32,7 +32,7 @@ return (
         </Card.Body>
         </Card>
         
-        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black"}}>
+        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black",backgroundColor: "black", borderStyle: "solid", borderColor: "orange", borderWidth: "5px"}}>
         <Card.Img variant="top" src="/Silver.png" />
         <Card.Body>
             <Card.Title>2nd Runner Up</Card.Title>
@@ -51,7 +51,7 @@ return (
         </Card.Body>
         </Card>
 
-        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black" }}>
+        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black", backgroundColor: "black", borderStyle: "solid", borderColor: "orange", borderWidth: "5px" }}>
         <Card.Img variant="top" src="/Bronze.png" />
         <Card.Body>
             <Card.Title>3rd Runner Up</Card.Title>
@@ -69,12 +69,12 @@ return (
             </ul>
         </Card.Body>
         </Card>
-        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black" }}>
+        <Card style={{ width: '18rem', height: "35rem", backgroundColor: "black", backgroundColor: "black", borderStyle: "solid", borderColor: "orange", borderWidth: "5px" }}>
         <Card.Img variant="top" src="/number4-10.png" />
         <Card.Body>
             <Card.Title>4-10 Runner Up</Card.Title>
             <Card.Text>
-            NFT 4-10 RUNNER UP PRIZE
+            NFT 4-10 RUNNER 
             </Card.Text>
             <ul>
                 1. Gold Crown NFT 
